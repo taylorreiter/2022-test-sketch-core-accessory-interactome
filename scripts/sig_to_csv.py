a469c2db58f8bcc2f0c0191cb0d7dba83dd9a888
Taylor Reiter <taylorreiter@gmail.com>
2022-03-23 16:39:13 -0600

    add new sig to csv script

---
+++ scripts/sig_to_csv.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Given a sourmash signature, extract the minhash integers and abund info
+to a csv file with the basename of the sig file name as the name of the
+abund column.
+"""
+
+from sourmash import signature
+import pandas as pd
+import os
+import sys
+import argparse
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('signature')       # sourmash signature
+    p.add_argument('output')          # output csv file name
+    args = p.parse_args()
+
+    # load the signature from disk
+    sigfp = open(args.signature, 'rt')
+    siglist = list(signature.load_signatures(sigfp))
+    loaded_sig = siglist[0]
+
+    # Get the minhashes and abundances
+    mins  = loaded_sig.minhash.hashes
+    name = os.path.basename(args.signature)
+    df = pd.DataFrame(mins.values(), mins.keys(), columns = [name])
+
+    # write to a csv
+    df.to_csv(args.output, index_label= "minhash")
+
+if __name__ == '__main__':
+    sys.exit(main())
